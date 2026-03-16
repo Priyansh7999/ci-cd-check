b1374ba6741494a15da84bec97b06abb10052149
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-16 13:59:05 +0530

    fix: remove unused import

--- test_src/App.test.tsx
+++ test_src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from "react-router-dom"
 import App from "../src/App"
 import { describe, expect, test } from 'vitest'
